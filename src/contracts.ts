0b967f92a9b5365b4e770ff103013003b6d094fe
unknown <10414642+Roaders@users.noreply.github.com>
2020-05-21 13:08:10 +0100

    Add cli help

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,9 +1,10 @@
-import { PingResponse } from "ping";
+import { PingResponse } from 'ping';
 
 export interface ICommandLineArgs {
     range: string;
     name?: string;
     vendor?: string;
+    help?: string;
 }
 
 export interface IIpVendor extends PingResponse {
--- src/index.ts
+++ src/index.ts
@@ -17,55 +17,88 @@ import { from, defer } from 'rxjs';
 import { mergeMap, filter, share, toArray, scan, combineLatest } from 'rxjs/operators';
 import { promise as pingPromise } from 'ping';
 import { getTable } from '@network-utils/arp-lookup';
-
-const commandOptions: Partial<ICommandLineArgs> = commandLineArgs([
-    { name: 'range', alias: 'r' },
-    { name: 'vendor', alias: 'v' },
-    { name: 'name', alias: 'n' },
-]) as Partial<ICommandLineArgs>;
+import commandLineUsage, { OptionDefinition, Section } from 'command-line-usage';
 
 const defaultOptions: ICommandLineArgs = {
     range: getDefaultRange(networkInterfaces()),
 };
+
+const args: OptionDefinition[] = [
+    { name: 'range', alias: 'r', description: `Optional. IP range to scan. Defaults to ${defaultOptions.range}` },
+    {
+        name: 'vendor',
+        alias: 'v',
+        typeLabel: 'regular expression',
+        description: `Optional. Filters the list matching vendor to the supplied regular expression`,
+    },
+    {
+        name: 'name',
+        alias: 'n',
+        typeLabel: 'regular expression',
+        description: `Optional. Filters the list matching network name to the supplied regular expression`,
+    },
+    { name: 'help', alias: 'h', type: Boolean, description: 'print this usage guide' },
+];
+
+const commandOptions: Partial<ICommandLineArgs> = commandLineArgs(args) as Partial<ICommandLineArgs>;
 
 const options = { ...defaultOptions, ...commandOptions };
 
 const nameRegExp = options.name ? new RegExp(options.name) : undefined;
 const vendorRegExp = options.vendor ? new RegExp(options.vendor) : undefined;
 
-if (!verifyRange(options.range)) {
-    throw new Error(`Range '${options.range}'is not in correct format: '192.168.0.[0-255]'`);
-}
+function scanIps() {
+    if (!verifyRange(options.range)) {
+        throw new Error(`Range '${options.range}'is not in correct format: '192.168.0.[0-255]'`);
+    }
 
-console.log(printWelcomeMessage(options));
+    console.log(printWelcomeMessage(options));
 
-const ipStream = from(expandRange(options.range)).pipe(
-    mergeMap((ip) => from(pingPromise.probe(ip, { timeout: 1, numeric: true })), 100),
-    filter((result) => result.alive),
-    mergeMap(getIpName),
-    filter((result) => (nameRegExp != null ? nameRegExp.test(result.host) : true)),
-    share(),
-);
-
-let ipCount = 0;
-let vendors: IIpVendor[] = [];
-const ipTableStream = defer(() => from(getTable()));
-
-ipStream
-    .pipe(
-        toArray(),
-        combineLatest(ipTableStream),
-        mergeMap(([responses, arpTable]) => getVendors(responses, arpTable), 1),
-        filter((ip) => (vendorRegExp != null ? vendorRegExp.test(ip.vendor) : true)),
-        scan((all, vendor) => [...all, vendor], new Array<IIpVendor>()),
-    )
-    .subscribe(
-        (results) => {
-            vendors = results;
-            logProgress(ipCount, vendors);
-        },
-        undefined,
-        () => printResultsTable(vendors),
+    const ipStream = from(expandRange(options.range)).pipe(
+        mergeMap((ip) => from(pingPromise.probe(ip, { timeout: 1, numeric: true })), 100),
+        filter((result) => result.alive),
+        mergeMap(getIpName),
+        filter((result) => (nameRegExp != null ? nameRegExp.test(result.host) : true)),
+        share(),
     );
 
-ipStream.pipe().subscribe(() => logProgress(++ipCount));
+    let ipCount = 0;
+    let vendors: IIpVendor[] = [];
+    const ipTableStream = defer(() => from(getTable()));
+
+    ipStream
+        .pipe(
+            toArray(),
+            combineLatest(ipTableStream),
+            mergeMap(([responses, arpTable]) => getVendors(responses, arpTable), 1),
+            filter((ip) => (vendorRegExp != null ? vendorRegExp.test(ip.vendor) : true)),
+            scan((all, vendor) => [...all, vendor], new Array<IIpVendor>()),
+        )
+        .subscribe(
+            (results) => {
+                vendors = results;
+                logProgress(ipCount, vendors);
+            },
+            undefined,
+            () => printResultsTable(vendors),
+        );
+
+    ipStream.pipe().subscribe(() => logProgress(++ipCount));
+}
+
+if (options.help) {
+    const sections: Section[] = [
+        {
+            header: 'IP Vendor Scanner',
+            content: 'Scans a range of IP addresses and returns a list of network names and vendors',
+        },
+        {
+            header: 'Options',
+            optionList: args,
+        },
+    ];
+
+    console.log(commandLineUsage(sections));
+} else {
+    scanIps();
+}
